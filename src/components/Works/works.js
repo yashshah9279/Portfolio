edcdff84f3ae74f06a9a503ab8fa790f08a19b15
Yash <yashshah@gmail.com>
2024-01-29 19:13:17 +0530

    updated works

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './works.css';
 import Portfolio1 from '../../assets/Travel Tracker.png';
 import Portfolio2 from '../../assets/TodoList.png';
-import Portfolio3 from '../../assets/FlagQuiz.png';
+import Portfolio3 from '../../assets/Restaurant.png';
 import Portfolio4 from '../../assets/Snakegame.png';
 // import Portfolio5 from '../../assets/portfolio-5.png';
 // import Portfolio6 from '../../assets/portfolio-6.png';
@@ -12,6 +12,13 @@ const Works = () => {
         <h2 className="worksTitle">My Portfolio</h2>
         <span className="workDesc">I take pride in paying attention to smallest details </span>
         <div className="worksImgs">
+        <div className="porfolio3">
+   <img src={Portfolio3} alt="Portfolio3" className="worksImg" />
+   <p className="portfolioText">
+      <a href="https://github.com/yashshah9279/restaurant.git" className="gitLink" style={{textDecoration:"none",color:"white"}}>Restaurant Website
+      </a>
+    </p>
+</div>
         <div className="portfolio1">
         <img src={Portfolio1} alt="Portfolio1" className="worksImg" />
             <p className="portfolioText"> 
@@ -25,13 +32,6 @@ const Works = () => {
           </a>
           </p>
         </div>
-<div className="porfolio3">
-   <img src={Portfolio3} alt="Portfolio3" className="worksImg" />
-   <p className="portfolioText">
-      <a href="https://github.com/yashshah9279/Flag-Quiz.git" className="gitLink" style={{textDecoration:"none",color:"white"}}>Flag Quiz
-      </a>
-    </p>
-</div>
 <div className="porfolio4">
     <img src={Portfolio4} alt="Portfolio4" className="worksImg" />
     <p className="portfolioText">
